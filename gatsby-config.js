dcf56630c92bf8f12811f3dbf896ccf515aeaf8b
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2022-01-20 13:00:47 +0100

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,53 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-      title: `canzoniere_dani`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `canzoniere_dani`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: `danielebonaiuto`,
+        accessToken: `${process.env.PRISMIC_ACCESS_TOKEN}`,
+        linkResolver: (doc) => linkResolver(doc),
+        schemas: {
+          song: require("./src/schemas/song.json"),
+          album: require("./src/schemas/album.json"),
+        },
+      },
+    },
+  ],
 };
